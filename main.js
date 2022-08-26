1b83411d1a1e4117e67bd7f81be38c046b2baace
mihkuno <caindayjoeninyo@gmail.com>
2022-08-26 11:11:51 +0800

    added thread listener

--- main.js
+++ main.js
@@ -1,11 +1,62 @@
 const login = require("fb-chat-api");
 const fs = require('fs');
 
-// Create simple echo bot
-login({ appState: JSON.parse(fs.readFileSync("fbcookies.json", "utf-8")).cookies } , (err, api) => {
+// use cookie extractor extension to log in
+const credentials = { appState: JSON.parse(fs.readFileSync("fbcookies.json", "utf-8")).cookies } 
+login(credentials, (err, api) => {
+    
+    // check for errors when logging in
     if(err) return console.error(err);
 
-    var yourID = "000000000000000";
-    var msg = "Hey! joe wassup imma go now";
-    api.sendMessage(msg, yourID);
+    // allow listening and self events
+    api.setOptions({listenEvents: true});
+    api.setOptions({selfListen: true})
+
+    // chatbot event listener
+    const listen = api.listen((err, event) => {
+        // check for listening errors
+        if(err) return console.error(err);
+
+        // mark read as soon as event recieved
+        api.markAsRead(event.threadID, (err) => {
+            if(err) console.error(err);
+        });
+
+        // check what thread event-type recieved
+        switch(event.type) {
+
+            // check chat thread information
+            case "message": 
+                api.getThreadInfo(event.threadID, (err, dt) => {
+                    if(err) console.error(err);
+                    
+                    
+                    console.log(dt);
+                    console.log('-----------------------');
+                    
+                    // give host superuser commands
+                    if(dt.snippetSender === '100004964793970') { 
+                        if ( event.body === '>stop()') {
+                            api.sendMessage("-- NODECS DEACTIVATED --", event.threadID);
+                            return listen.stopListening();
+                        }
+                    }
+
+                    // listening to group chat
+                    if (dt.isGroup) {
+                        // api.sendMessage(event.body, event.threadID);
+                    }
+                    // listening to user chat
+                    else {
+                        
+                    }
+                })
+            break;
+   
+            // miscellaneous events (added, nickname, etc)
+            case "event":
+                console.log(event);
+            break;
+        }
+    });
 });
